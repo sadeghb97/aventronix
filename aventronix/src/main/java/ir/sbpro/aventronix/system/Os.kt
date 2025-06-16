923815f6a0798373d89074f1aca8f15b70ec9a8b
sadeghb97 <sadeghb97@gmail.com>
2025-06-16 12:14:44 +0330

    update transparent navigation bar for android 14

--- aventronix/src/main/java/ir/sbpro/aventronix/system/Os.kt
+++ aventronix/src/main/java/ir/sbpro/aventronix/system/Os.kt
@@ -47,22 +47,28 @@ fun HideNavigationBar() {
     }
 }
 
+@SuppressLint("ContextCastToActivity")
 @Composable
 fun TransparentNavigationBar() {
     val activity = LocalContext.current as? Activity ?: return
 
+    val window = activity.window
+    val decorView = window.decorView
+
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
-        WindowCompat.setDecorFitsSystemWindows(activity.window, false)
-        activity.window.setDecorFitsSystemWindows(false)
-        activity.window.navigationBarColor = TRANSPARENT
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.setNavigationBarColor(TRANSPARENT)
+        WindowInsetsControllerCompat(window, decorView).apply {
+            isAppearanceLightNavigationBars = false
+        }
     } else {
         @Suppress("DEPRECATION")
-        activity.window.decorView.systemUiVisibility = (
-                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
-                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                )
+        decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+
         @Suppress("DEPRECATION")
-        activity.window.navigationBarColor = TRANSPARENT
+        window.navigationBarColor = TRANSPARENT
     }
 }
 
